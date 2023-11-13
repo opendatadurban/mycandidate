83b8c201bd076d59f8def30dcbc5f0b30c3960a6
tmotshabi <tshiamo@opencitieslab.org>
2023-11-13 14:26:27 +0200

    Updated migrations

--- tests/test_seeding.py
+++ tests/test_seeding.py
@@ -14,7 +14,7 @@ def test_seed_candidates_from_excel(client):
         # Perform assertions
         num_candidates = db.session.query(Candidate).count()
         # print(">>>>>>>>", num_candidates)
-        expected_num_candidates = 18
+        expected_num_candidates = 17
         assert num_candidates == expected_num_candidates
 
 
