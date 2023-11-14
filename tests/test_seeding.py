9a9fd272044d3d95aba25f8dc0bc4aa1479a12b5
tmotshabi <tshiamo@opencitieslab.org>
2023-11-14 11:29:43 +0200

    Final seeding on workflow & heroku

--- tests/test_seeding.py
+++ tests/test_seeding.py
@@ -14,7 +14,7 @@ def test_seed_candidates_from_excel(client):
         # Perform assertions
         num_candidates = db.session.query(Candidate).count()
 
-        expected_num_candidates = 19
+        expected_num_candidates = 21
         assert num_candidates == expected_num_candidates
 
 
